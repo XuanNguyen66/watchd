35539b1e5f32f25458b1fd9940a51f872eed564e
XuanNguyen66 <nguyenthixuann@gmail.com>
2026-06-11 22:41:33 +0700

    wrong trim

--- parser.c
+++ parser.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+void trim(char *str){
+    // Find the space on the right
+    int len = strlen(str);
+    while (len > 0 && isspace((unsigned char)str[len - 1])){
+        len--;
+    }
+    str[len] = '\0';
+    
+    //Find the space on the left
+    int start = 0;
+    while(str[start] != '\0' && isspace((unsigned char) str[start])){
+        start++;
+    }
+    if(start > 0){
+        memmove(str, str + start, len - start + 1);
+    }
+}
+
 
 
 int main(){
@@ -11,8 +32,12 @@ int main(){
         perror("Error opening file");
         exit(0);
    }
-   while(fgets(line, sizeof(line), file)){
-        printf("%s", line);
+  // int line_num = 0;
+
+   while(fgets(line, sizeof(line), file) != NULL){
+    //    line_num++;
+        trim(line);
+        printf("%s\n", line);
    }
 
    fclose(file);
