6261206a30fa5ff642279120cf056b65caf45ea3
XuanNguyen66 <nguyenthixuann@gmail.com>
2026-06-12 22:46:36 +0700

    update trim

--- src/trim.c
+++ src/trim.c
@@ -4,7 +4,29 @@
 
 
 void trimline(char *str){
-        
+     if (str == NULL)
+         return;
+     int len = strlen(str);
+     if(len == 0)
+         return;
+     int end = len - 1;
+     if(end <= 0 && isspace((unsigned char)str[end])){
+        end--;
+     }
+     str[end + 1] = '\0';
 
+     int start = 0;
+     while(str[start] != '\0' && isspace((unsigned char)str[start]){
+        start++;
+     }
+
+     if(start > 0){
+        int i = 0;
+        while(start[start + i] != '\0'){
+            str[i] = str[start + i];
+            i++
+        }
+        str[i] = '\0';
+     }
 }
 
